94af3b1d3f3cb766ef42a108a2739426c35aa823
hulki <muhamadamin1992@gmail.com>
2019-07-06 12:01:43 +0500

    Implement money exchange with loop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports = function makeExchange( currency ) {
+  'use strict';
+
+  const COINS = [ 'H', 'Q', 'D', 'N', 'P' ],
+  			COINSVALUES = [ 50, 25, 10, 5, 1 ];
+  			
+  let res = {},
+  		i = 0;
+
+  if ( currency > 10000  ) {
+  	res.error = "You are rich, my friend! We don't have so much coins for exchange";
+  	return res;
+  }
+
+  while ( currency > 0 ) {
+  	let count = Math.floor( currency / COINSVALUES[i] );
+
+		if ( count > 0 ) {
+			currency -= COINSVALUES[i] * count;
+			res[COINS[i]] = count;
+		}
+
+		i++;
+  }
+
+  return res;
 }
